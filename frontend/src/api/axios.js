987c1cc1decbbaafa055473df63994e4ef655a1b
kishore-stack <s.kishorekumar.mech@gmail.com>
2026-02-02 10:28:51 +0530

    fix: use env-based API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-instance.interceptors.request.use(config => {
+instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
